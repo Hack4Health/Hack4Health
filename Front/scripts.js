0a67ad2fd6b86bb6b36420bdd461b2d93b1d3bc5
Lenic Zhang <lenic.zhang@gmail.com>
2021-05-22 21:31:06 -0700

    adding side panel

---
+++ Front/scripts.js
@@ -0,0 +1,15 @@
+function openNav() {
+  document.getElementById("mySidepanel").style.width = "300px";
+}
+
+function closeNav() {
+  document.getElementById("mySidepanel").style.width = "0";
+}
+
+function addingGoal() {
+  document.getElementById("addingGoals").style.width = "300px";
+}
+
+function closeGoal() {
+  document.getElementById("addingGoals").style.width = "0";
+}
